4ad28914e24f6f2e3e7e8730cd7891fa904a726c
Hagop Alajajian <114875390+hagop-a@users.noreply.github.com>
2026-06-14 22:10:56 -0400

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,61 @@
+class Solution:
+    def stackChange(self, stack, i):
+        if stack and stack[-1] == i:
+            stack.pop()
+            return True
+        return False
+
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 == 1:
+            return False
+
+        stack = []
+        for c in s:
+            if c == '}':
+                i = '{'
+                if not self.stackChange(stack,i):
+                    return False
+            elif c == ')':
+                i = '('
+                if not self.stackChange(stack,i):
+                    return False
+            elif c == ']':
+                i = '['
+                if not self.stackChange(stack,i):
+                    return False
+        
+            #if stack and stack[-1] == i:
+            #    stack.pop()
+            #else:
+            #    return False
+
+            if (c == '(' or c == '{' or c == '['):
+               stack.append(c)
+        return len(stack) == 0
+
+
+
+        #pl = 0
+        #pr = 1
+        #   
+        #while pl < pr and pr < len(s):
+        #    countPairs = 0
+        #    while pr < len(s) and s[pr] in "({[":                
+        #        pr+= 1 # go towards closed bracket
+        #        countPairs+= 1 
+        #        if pr < len(s) and s[pr] in ")}]":
+        #            pr += countPairs
+        #            break
+                #if s[pr] in ")}]":
+                #    pr += countPairs #now outside of pairs
+
+            # check if brackets match
+        #    for i in range(countPairs):
+         #       if ((s[pl] == '{' and s[pr] != '}') or (s[pl] == '(' and s[pr] != ')') or (s[pl] == '[' and s[pr] != ']')):
+        #            return False
+        #        pl += 1
+        #        pr -= 1
+        #    pl += countPairs + 1
+        #    pr += countPairs + 1
+        #return True
+
